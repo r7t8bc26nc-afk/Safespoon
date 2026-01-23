59c29072192acb6a2385fc27dcd9217e84ac889a
Quajaee <43022366+Quajaee@users.noreply.github.com>
2026-01-22 23:33:05 -0500

    v2 production

--- client/src/firebase.js
+++ client/src/firebase.js
@@ -1,17 +1,13 @@
-// src/firebase.js
+// DELETE THIS (Admin/Service Account)
+// import serviceAccount from './serviceAccountKey.json';
+// admin.initializeApp({ credential: admin.credential.cert(serviceAccount) });
+
+// USE THIS INSTEAD (Safe for Client)
 import { initializeApp } from "firebase/app";
-import { getFirestore } from "firebase/firestore";
-
-// TODO: Replace the following with your app's Firebase project configuration
-// You can get this from Firebase Console > Project Settings > General > Your Apps > SDK Setup
 const firebaseConfig = {
-    apiKey: "AIzaSyBNZ67JjwoFIXXDg5OK6w9SuabhQmzZ7aw",
-  authDomain: "vensight-bfde4.firebaseapp.com",
-  projectId: "vensight-bfde4",
-  storageBucket: "vensight-bfde4.firebasestorage.app",
-  messagingSenderId: "149066473193",
-  appId: "1:149066473193:web:8ba0b6b75866a9923fda78",
+  apiKey: "YOUR_API_KEY",
+  authDomain: "YOUR_PROJECT.firebaseapp.com",
+  projectId: "YOUR_PROJECT",
+  // ... other config values from Firebase Console
 };
-
 const app = initializeApp(firebaseConfig);
-export const db = getFirestore(app);
